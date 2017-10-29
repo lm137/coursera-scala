7a4fc71b47b94ff96fec8d54fc49def2df4b9f16
lm137 <lewis.s.morgan@gmail.com>
2017-10-29 22:28:58 +0000

    Added FunctionalProgrammingPrinciples project (inc. example code)

---
+++ FunctionalProgrammingPrinciples/.worksheet/src/coursera.course1.week1.scratch.scala
@@ -0,0 +1,5 @@
+package coursera.course1.week1
+
+object scratch {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(92); 
+  println("Welcome to the Scala worksheet")}
+}
---
+++ FunctionalProgrammingPrinciples/src/coursera/course1/week1/scratch.sc
@@ -0,0 +1,5 @@
+package coursera.course1.week1
+
+object scratch {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+}
